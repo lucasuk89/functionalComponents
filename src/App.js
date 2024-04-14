b2a819631f078b1463a42378a0fb4fc298871512
lucasalumnocei <lucasuk89@alumnos.cei.es>
2024-04-14 09:35:28 +0200

    Created a basic functional components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
+
+import React,{useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App(){
+const [saludo,setSaludo] = useState(1)
 
-export default App;
+useEffect(()=>{
+  console.log("Vamos aumentar?")
+},[saludo])
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            target="_blank"
+            rel="noopener noreferrer"
+            onClick={()=> setSaludo (saludo+1)}       
+          >
+        {saludo}
+          </a>
+        </header>
+      </div>
+    );
+  }
+
+
+
+  // https://www.youtube.com/watch?v=Bx7taFW0XbY
+
+  
